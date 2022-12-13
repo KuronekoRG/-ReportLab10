7a658d208265f9ef583bf8ea86cb86954ac4e9d1
KuronekoRG <120499564+KuronekoRG@users.noreply.github.com>
2022-12-13 22:12:24 +0300

    Update Participation.java

--- mispis_lab10/src/ru/vsu/staver/Participation.java
+++ mispis_lab10/src/ru/vsu/staver/Participation.java
@@ -1,7 +1,14 @@
 package ru.vsu.staver;
 
 public class Participation {
+        private int hours;
 
-	public int hours;
+	public int getHours() {
+		return hours;
+	}
+
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
 
 }
