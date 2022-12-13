4fd9c2b0e64648a58694484012b888c90726fa82
KuronekoRG <120499564+KuronekoRG@users.noreply.github.com>
2022-12-13 22:07:31 +0300

    Update Course.java

--- mispis_lab10/src/ru/vsu/staver/Course.java
+++ mispis_lab10/src/ru/vsu/staver/Course.java
@@ -2,28 +2,58 @@ package ru.vsu.staver;
 
 import java.util.*;
 
+
 public class Course {
 
-	public Course(Collection<Lecturer> teaches, String name, int id, float hours) {
-		this.teaches = teaches;
-		this.name = name;
-		this.id = id;
-		this.hours = hours;
-	}
+    public Course(Collection<Lecturer> teaches, String name, int id, float hours) {
+        this.lecturers = teaches;
+        this.name = name;
+        this.id = id;
+        this.hours = hours;
+    }
 
-	Collection<Lecturer> teaches;
-	public String name;
-	public int id;
-	public float hours;
+    private Collection<Lecturer> lecturers;
+    private String name;
+    private int id;
+    private float hours;
 
-	public void assignStudentToCourse(){
+    public void addLecturerToCourse() {
 
-	}
-	public void unsubscribeStudentFromCourse(){
+    }
 
-	}
-	public void giveStudentGradeForCourse(){
+    public void removeLecturerFromCourse() {
 
-	}
+    }
 
+    public Collection<Lecturer> getLecturers() {
+        return lecturers;
+    }
+
+    public void setLecturers(Collection<Lecturer> lecturers) {
+        this.lecturers = lecturers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getHours() {
+        return hours;
+    }
+
+    public void setHours(float hours) {
+        this.hours = hours;
+    }
 }
