4acd7682f18a4400faf4c4b0303d616470df8cae
KuronekoRG <120499564+KuronekoRG@users.noreply.github.com>
2022-12-13 22:11:50 +0300

    Update Lecturer.java

--- mispis_lab10/src/ru/vsu/staver/Lecturer.java
+++ mispis_lab10/src/ru/vsu/staver/Lecturer.java
@@ -2,16 +2,4 @@ package ru.vsu.staver;
 
 public class Lecturer extends Research_Associate {
 
-    public void readLecture(){
-
-    }
-
-    public void openAudience(){
-
-    }
-
-    public void closeAudience(){
-
-    }
-
 }
